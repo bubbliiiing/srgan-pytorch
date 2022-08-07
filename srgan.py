c9b84314b9fbeea114525bef909861ed790aae36
Bubbliiiing <3323290568@qq.com>
2022-08-08 00:07:31 +0800

    update detect_image

--- srgan.py
+++ srgan.py
@@ -49,7 +49,7 @@ class SRGAN(object):
             cudnn.benchmark = True
             self.net = self.net.cuda()
         
-    def generate_1x1_image(self, image):
+    def detect_image(self, image):
         #---------------------------------------------------------#
         #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
         #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
